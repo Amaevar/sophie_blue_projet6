7ad62d790ae38b58dabe5fe5e018fbe34e298918
aiche maeva <aiche.maeva98@gmail.com>
2026-02-19 16:55:35 +0300

    commentaire loging.js

--- FrontEnd/loging.js
+++ FrontEnd/loging.js
@@ -1,14 +1,22 @@
+
+// --- ÉTAPE 1 : RÉCUPÉRATION DES ÉLÉMENTS DU DOM ---
+
 const form = document.querySelector(".login-form");
-const errorMsg = document.querySelector(".error"); 
+const errorMsg = document.querySelector(".error");
+
+
+// --- ÉTAPE 2 : ÉCOUTE DE L'ÉVÉNEMENT ---
 
 form.addEventListener("submit", async function (e) {
     e.preventDefault();
 
-    
+
     if (errorMsg) errorMsg.innerText = "";
 
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
+
+// --- ÉTAPE 3 : APPEL À L'API (REQUÊTE RÉSEAU) ---
 
     try {
         const response = await fetch("http://localhost:5678/api/users/login", {
@@ -22,17 +30,21 @@ form.addEventListener("submit", async function (e) {
             })
         });
 
+// --- ÉTAPE 4 : TRAITEMENT DE LA RÉPONSE ---
+
         if (response.ok) {
             // Cas où ces correct
             const data = await response.json();
             localStorage.setItem("token", data.token);
             window.location.href = "index.html";
         } else {
-            // Cas 401 ou 404
+            // Cas où ces faut
             errorMsg.innerText = "Email ou mot de passe incorrect";
         }
     } catch (err) {
-        // Cas où le serveur ne répond pas
+
+ // Cas où le serveur ne répond pas
         errorMsg.innerText = "Impossible de joindre le serveur.";
-    } 
+    }
 });
+
